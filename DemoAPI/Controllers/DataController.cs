f1209463b08507895f013c252de46f615740505a
Afzal Rahim <afzalarahim@gmail.com>
2019-12-16 20:15:04 +0530

    Initial version

---
+++ DemoAPI/Controllers/DataController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+
+        public string Get()
+        {
+            return "call api in 'api/data/<input>' format";
+        }
+        // GET: api/Data/5
+        [HttpGet("{id}", Name = "Get")]
+        public DataResult Get(int id)
+        {
+            var result = new DataResult { Success = true, Message = null };
+            if (id == 0)
+            {
+                result.Message = "input is zero!";
+                result.Result = id.ToString();
+            }
+            var resultText = "";
+            if (id % 7 == 0)
+            {
+                resultText = "C";
+            }
+            if (id % 9 == 0)
+            {
+                resultText += "N";
+            }
+           
+            if (resultText == "")
+            {
+                result.Message = "input is not a multiple of 7 or 9!";
+                resultText = id.ToString();
+            }
+
+            result.Result = resultText;
+
+            return result;
+        }
+
+        
+    }
+}
---
+++ DemoAPI/Model/DataResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoAPI.Model
+{
+    public class DataResult
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public string Result { get; set; }
+    }
+}
